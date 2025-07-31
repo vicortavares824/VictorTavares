28cc3ab70b89d9aebb60dd238d7fd2fc5d6f847b
vicortavares824 <vitinhotavares824@gmail.com>
2025-07-30 21:40:00 -0300

    mudar nome

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,8 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Victor Tavares',
+  description: 'Portfolio of Victor Tavares, a developer with a passion for creating immersive experiences.',
 }
 
 export default function RootLayout({
